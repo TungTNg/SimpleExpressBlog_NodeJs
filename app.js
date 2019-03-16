8822586e65ea0bc40da7c179f4d72476f867082a
Tung Nguyen <mecer80@gmail.com>
2019-03-16 20:55:07 +0000

    Added EDIT & UPDATE Routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express     = require('express'),
-    app         = express(),
-    bodyParser  = require('body-parser'),
-    mongoose    = require('mongoose');
+var express         = require('express'),
+    methodOverride  = require('method-override'),
+    app             = express(),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose');
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/simple_express_blog", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -62,6 +64,28 @@ app.get("/blogs/:id", function(req, res) {
             res.redirect("/SimpleExpressBlog/blogs");
         } else {
             res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/SimpleExpressBlog/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+            res.redirect("/SimpleExpressBlog/blogs");
+        } else {
+            res.redirect("/SimpleExpressBlog/blogs/req.params.id");
         }
     });
 });
